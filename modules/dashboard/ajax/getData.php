5ed2efdd1cfa9f42a92b11e15b5e95a4b0c42f02
Tara Campbell <tara0campbell@gmail.com>
2017-02-27 11:17:46 +0100

    First draft of getTableData.

--- modules/dashboard/ajax/getData.php
+++ modules/dashboard/ajax/getData.php
@@ -52,10 +52,8 @@ function getVisitLabels() {
 }
 
 function getTableData() {
-    $DB = Database::singleton();
-
     $candidates = $DB->pselect(
-        "SELECT c.PSCID, psc.Name
+        "SELECT c.PSCID, c.CandID, psc.Name
          FROM candidate c
          LEFT JOIN psc USING (CenterID)
          WHERE c.Active='Y' AND c.Entity_type='human'"
@@ -65,13 +63,66 @@ function getTableData() {
 
     foreach ($candidates as $candidate) {
         $pscid  = $candidate['PSCID'];
+        $candID = $candidate['CandID'];
         $psc    = $candidate['Name'];
         $visits = array();
+
+        $sessionInfo = $DB->pselect(
+            "SELECT ID, Visit_label, SubprojectID, Submitted, Date_visit
+             FROM session
+             WHERE CandID=:CID",
+            array('CID' => $candID)
+        );
+
+        foreach ($sessionInfo as $session) {
+            $sessionID  = $session['ID'];
+            $subproject = $session['SubprojectID'];
+            $visitLabel = $session['Visit_label']
+
+            $visit = array();
+            $visit['sessionID'] = $sessionID;
+            $visit['visitRegStatus'] = 'no-deadline-visit';
+            $visit['dataEntryStatus'] = 'no-deadline-visit';
+            $visit['visitRegDueDate'] = null;
+            $visit['dataEntryDueDate'] = null;
+            $visit['instrCompleted'] = getTotalInstrumentsCompleted($sessionID);
+            $visit['totalInstrs'] = getTotalInstruments($visitLabel, $subproject);
+            $visit['visitLabel'] = $visitLabel;
+            $visit['cohort'] = $subproject;
+
+            array_push($visits, $visit);
+        }
 
         array_push($tableData, array('pscid' => $pscid, 'psc' => $psc, 'visits' => $visits));
     }
 
     return $tableData;
+}
+
+function getTotalInstruments($visitLabel, $subproject) {
+    global $DB;
+
+    $totalInstruments = $DB->pselect(
+        "SELECT COUNT(ID)
+         FROM test_battery
+         WHERE SubprojectID=:CID AND Visit_label=:V",
+        array('CID' => $candID)
+    );
+
+    return $totalInstruments;
+}
+
+function getTotalInstrumentsCompleted($sessionID) {
+    global $DB;
+
+    $totalInstruments = $DB->pselect(
+        "SELECT COUNT(ID)
+         FROM flag
+         WHERE SessionID=:SID AND Data_entry='Complete'",
+        array('SID' => $sessionID)
+    );
+
+    return $totalInstruments;
 }
 
 ?>

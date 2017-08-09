1693592c43a893f3120598f7536c37399b4b85e2
Tara Campbell <tara0campbell@gmail.com>
2017-08-09 13:12:36 -0400

    full rebase
    major changes to the script
    
    Major changes
    
    Everything works
    
    first of many
    
    renamed
    
    renamed

---
+++ SQL/Archive/2017-03-10-addUserAccountsRoles.sql
@@ -0,0 +1,29 @@
+ALTER TABLE `permissions` DROP FOREIGN KEY `fk_permissions_1`;
+ALTER TABLE `permissions` DROP INDEX `fk_permissions_1_idx`;
+ALTER TABLE `permissions` DROP COLUMN `categoryID`;
+
+DROP TABLE `permissions_category`;
+
+CREATE TABLE `permission_category` (
+ `PermissionCategoryID` INTEGER unsigned NOT NULL AUTO_INCREMENT,
+ `Category` varchar(255),
+ `Label` varchar(255),
+ PRIMARY KEY (`PermissionCategoryID`),
+ UNIQUE KEY `UK_Category` (`Category`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `permission_category_permissions_rel` (
+  `PermissionCategoryID` INTEGER unsigned NOT NULL,
+  `PermissionID` INTEGER unsigned NOT NULL,
+  PRIMARY KEY  (`PermissionCategoryID`,`PermissionID`),
+  CONSTRAINT `FK_permission_category_permissions_rel_PermissionCategoryID` FOREIGN KEY (`PermissionCategoryID`) REFERENCES `permission_category` (`PermissionCategoryID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_permission_category_permissions_rel_PermissionID` FOREIGN KEY (`PermissionID`) REFERENCES `permissions` (`permID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `users_permission_category_rel` (
+  `UserID` INTEGER unsigned NOT NULL,
+  `PermissionCategoryID` INTEGER unsigned NOT NULL,
+  PRIMARY KEY  (`UserID`,`PermissionCategoryID`),
+  CONSTRAINT `FK_users_permission_category_rel_userID` FOREIGN KEY (`userID`) REFERENCES `users` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_users_permission_category_rel_PermissionCategoryID` FOREIGN KEY (`PermissionCategoryID`) REFERENCES `permission_category` (`PermissionCategoryID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
